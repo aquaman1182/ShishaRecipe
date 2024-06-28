2f3a11e2cbc39eda9ba25d0455e1ea4cf1bf7463
aquaman1182 <hal.bandotakumi@gmail.com>
2024-06-28 09:35:20 +0900

    Initial Commit

---
+++ shisha_recipe/shisha_recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  shisha_recipeApp.swift
+//  shisha_recipe
+//
+//  Created by 拓実 on 2024/06/28.
+//
+
+import SwiftUI
+
+@main
+struct shisha_recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
